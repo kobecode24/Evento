ac3a3325101f7a5d10eded9f24a72c8ccd34350b
kobe <youcode@skiff.com>
2024-03-08 16:02:53 +0100

    added more routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\admin\CategoryController;
-use App\Http\Controllers\admin\EventController;
 use App\Http\Controllers\admin\UserController;
+use App\Http\Controllers\organizer\ReservationController as OrganizerReservationController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\user\EventController as userEventController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\admin\EventController as AdminEventController;
+use App\Http\Controllers\organizer\EventController as OrganizerEventController;
+use App\Http\Controllers\user\ReservationController as UserReservationController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -34,12 +37,31 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
-Route::prefix('admin')->group(function () {
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('statistics',[UserController::class, 'statistics'])->name('statistics');
     Route::get('/users/{user}/ban', [UserController::class, 'ban'])->name('users.ban');
     Route::get('/users/{user}/unban', [UserController::class, 'unban'])->name('users.unban');
     Route::resource('categories', CategoryController::class);
     Route::resource('users', UserController::class);
-    Route::resource('events', EventController::class);
-    Route::get('/events/{id}/accept', [EventController::class, 'accept'])->name('events.accept');
-    Route::get('/events/{id}/decline', [EventController::class, 'decline'])->name('events.decline');
+    Route::resource('events', AdminEventController::class);
+    Route::get('/events/{event}/accept', [AdminEventController::class, 'accept'])->name('events.accept');
+    Route::get('/events/{event}/decline', [AdminEventController::class, 'decline'])->name('events.decline');
+});
+
+Route::prefix('organizer')->name('organizer.')->group(function () {
+    Route::get('statistics',[OrganizerEventController::class, 'statistics'])->name('statistics');
+    Route::resource('events', OrganizerEventController::class);
+    Route::resource('reservations', OrganizerReservationController::class);
+    Route::get('/reservations/{reservation}/accept', [OrganizerReservationController::class, 'accept'])->name('reservations.accept');
+    Route::get('/reservations/{reservation}/decline', [OrganizerReservationController::class, 'decline'])->name('reservations.decline');
+    Route::get('/events/{event}/cancel', [OrganizerEventController::class, 'cancel'])->name('events.cancel');
+});
+
+Route::prefix('user')->name('user.')->group(function () {
+    Route::resource('events', userEventController::class);
+    Route::get('/reservations/create/{id}', [UserReservationController::class, 'create'])->name('reservations.create');
+    Route::resource('reservations', UserReservationController::class)->except(['create']);
+    Route::get('/download-ticket/{ticketId}', [UserReservationController::class,'ticket_download'])->name('ticket_download');
+
 });

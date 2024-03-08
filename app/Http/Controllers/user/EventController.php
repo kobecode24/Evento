579689abffa27a38b93ba583b0c5edbac43c1d1c
kobe <youcode@skiff.com>
2024-03-08 17:39:27 +0100

    added the search and Category filter

--- app/Http/Controllers/user/EventController.php
+++ app/Http/Controllers/user/EventController.php
@@ -3,17 +3,33 @@
 namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $events = Event::whereIn('status', [2, 3])->paginate(6);
-        return view('user.events.index' , compact('events'));
-    }
+        $categories = Category::all();
 
+        $query = Event::whereIn('status', [2, 3]);
+
+        if ($request->has('category_id') && $request->category_id != null) {
+            $query->where('category_id', $request->category_id);
+        }
+
+        if ($request->has('search') && $request->search != null) {
+            $query->where('title', 'like', '%' . $request->search . '%');
+        }
+
+        $events = $query->paginate(6)->appends([
+            'category_id' => $request->category_id,
+            'search' => $request->search,
+        ]);
+
+        return view('user.events.index', compact('events', 'categories'));
+    }
     public function create($id)
     {
         $event = Event::findOrFail($id);

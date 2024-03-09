92af6b755ee5da4152024c3334706dffcd4d89a0
kobe <youcode@skiff.com>
2024-03-09 20:31:13 +0100

    added the ban logic

---
+++ app/Http/Middleware/IsBanned.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsBanned
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check() && Auth::user()->status == 1) {
+            Auth::logout();
+
+            return redirect()->route('login')->with('error', 'Your account has been banned.');
+        }
+
+        return $next($request);
+    }
+}
